1580b9881975bddf74acb057127f82a792b3f607
kelin <lerbolario1225@gmail.com>
2019-05-19 13:32:56 +0800

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by kelinko on 2019/5/19.
+//  Copyright © 2019 kelinko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
